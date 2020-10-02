b0c95b7a415b3320d6ca57bbfdcf330056ff50ff
Sidany Walker <sidanyk@gmail.com>
2020-10-02 11:35:40 -0400

    GameTesting Part One

---
+++ GameTesting/GameElements.swift
@@ -0,0 +1,8 @@
+//
+//  GameElements.swift
+//  GameTesting
+//
+//  Created by Sidany Walker on 9/27/20.
+//
+
+import Foundation
--- GameTesting/GameViewController.swift
+++ GameTesting/GameViewController.swift
@@ -1,40 +1,32 @@
 //
 //  GameViewController.swift
-//  GameTesting
+//  Flappy Bao
 //
-//  Created by Sidany Walker on 9/27/20.
+//  Created by Sidany Walker on 9/26/20.
 //
 
 import UIKit
 import SpriteKit
-import GameplayKit
-
+ 
 class GameViewController: UIViewController {
-
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = false
+        skView.showsNodeCount = false
+        skView.ignoresSiblingOrder = false
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
             }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+ 
+   override var shouldAutorotate: Bool {
+        return false
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
+ 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -42,8 +34,14 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+ 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+ 
     override var prefersStatusBarHidden: Bool {
         return true
     }
 }
+
